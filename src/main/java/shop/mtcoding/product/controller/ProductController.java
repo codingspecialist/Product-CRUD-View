7288027cfb661bb890fdc18233ce5ee817daa51a
kimalss22 <kimalss22@gmail.com>
2023-05-19 12:21:58 +0900

    상품 리스트 Controller 및 View 구현
    list.mustache 파일 수정
    ProductController.java 파일 수정

--- src/main/java/shop/mtcoding/product/controller/ProductController.java
+++ src/main/java/shop/mtcoding/product/controller/ProductController.java
@@ -1,14 +1,25 @@
 package shop.mtcoding.product.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import shop.mtcoding.product.model.Product;
+import shop.mtcoding.product.model.ProductRepository;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductRepository productRepository;
+
     @GetMapping({"/product", "/"})
-    public String list() {
+    public String list(Model model) {
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute(productList);
         return "product/list";
     }
 
